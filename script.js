b14a0487b75b7c13633ca220ec3b39252ff926a9
SwiftCoderJoe <coderjoe05@gmail.com>
2021-09-13 20:17:21 -0400

    large if factual!

--- script.js
+++ script.js
@@ -1,54 +1,112 @@
 // Forward: normally I would add a ton of documentation comments but since DocC isn't standard (and this is javascript so types are literally meaningless) I won't.
 // Just regular comments for now.
 
+// why are classes not hoisted smh ! also this should definitely be a struct but js has no structs!!1! ! js is a very good and well thought out language
+class Color {
+    constructor(r, g, b, a) {
+        this.r = r
+        this.g = g
+        this.b = b
+        this.a = a
+        this.string = `rgba(${r}, ${g}, ${b}, ${a})`
+    }
+}
+
 window.addEventListener("load", (event) => {
 
     // Get a reference to the canvas
     let canvas = document.getElementById("mainCanvas")
 
-    console.log(canvas.width)
-
     // Get the canvas interface (Context)
     let interface = canvas.getContext(`2d`)
 
-    // Scale the canvas so that the accessible portion is always 800x600
+    // Scale the canvas so that the accessible portion is always 720p
     canvas.width = 1280
     canvas.height = 720
 
+    // Make lines rounded
     interface.lineCap = `round`
     interface.lineJoin = `round`
 
-    interface.fillStyle = `rgba(0, 0, 0, 0.5)`
-    drawCurve(0, 100, 0, 0, 100, 0, interface)
+    // Lines are black
+    interface.fillStyle = `rgb(0, 0, 0)`
 
+
+    // MARK: Fill the screen
+
+
+    startColor = new Color(255, 255, 0, 1)
+    endColor = new Color(255, 0, 255, 1)
+    drawVShape(0, 1350, 75, 800, 120, 2, 100, startColor, endColor)
+
+    startColor = new Color(0, 255, 255, 1)
+    endColor = new Color(255, 0, 255, 1)
+    drawVShape(0, 1200, 700, 0, 500, 1, 75, startColor, endColor)
+
+    // randomSpheres()
+
+
+    // MARK: Functions
+
+    // This function signature is absolutely insane and should use more objects. javascriopt moment!!!
+    function drawVShape(x1, x2, y1, y2, res, width, length, startColor, endColor) {
+        let xdiff = x2 - x1
+        let ydiff = y2 - y1
+
+        // Get slope and inverse slope to make the V-shapes
+        let slope = -ydiff/xdiff
+        let currentRad = Math.atan(slope)
+
+        // Color differences
+        let rdiff = endColor.r - startColor.r
+        let gdiff = endColor.g - startColor.g
+        let bdiff = endColor.b - startColor.b
+        let adiff = endColor.a - startColor.a
+
+        for (let i = 0; i < res; i++) {
+            // Linear distribution
+            let ratio = i/(res - 1)
     
+            // The starting coordinates along the linear distribution
+            x = x1 + ratio * xdiff
+            y = y1 + ratio * ydiff
+
+            // First offset point next to the current point along the linear distribution
+            let offsetX1 = x - Math.sin(currentRad - 5 * Math.PI/4) * length
+            let offsetY1 = y - Math.cos(currentRad - 5 * Math.PI/4) * length
+
+            // Second set of coordinates
+            let offsetX2 = x + Math.sin(currentRad + 5 * Math.PI/4) * length
+            let offsetY2 = y + Math.cos(currentRad + 5 * Math.PI/4) * length
+
+            interface.strokeStyle = new Color(startColor.r + ratio * rdiff, startColor.g + ratio * gdiff, startColor.b + ratio * bdiff, startColor.a + ratio * adiff).string
+            interface.lineWidth = width
+
+            // Draw the V
+            interface.beginPath()
+            interface.moveTo(offsetX1, offsetY1)
+            interface.lineTo(x, y)
+            interface.lineTo(offsetX2, offsetY2)
+            interface.stroke()
+        }
+    }
+
+    function randomSpheres() {
+        for (let i = 0; i < 70; i++) {
+            // string function wtf
+            interface.fillStyle = `rgba(${randomIn(255)}, ${randomIn(255)}, ${randomIn(255)}, 0.5)`
+    
+            let x = randomIn(canvas.width)
+            let y = randomIn(canvas.height)
+    
+            interface.beginPath()
+            interface.arc(x, y, randomIn(100), 0, Math.PI * 2)
+            interface.fill()
+        }
+    }
+
 })
 
 function randomIn(x) {
     return Math.floor(Math.random() * (x+ 1))
 }
-
-function drawCurve(x1, x2, y1, y2, res, width, interface) {
-    for (let i = 0; i < res; i++) {
-        let ratio = i/(res - 1)
-
-        x = x1 + ratio*(x1-x2)
-
-        console.log(`h`)
-        interface.fillRect(x, x, 10, 10)
-    }
-}
-
-/*
-for (let i = 0; i < 70; i++) {
-        // string function wtf
-        interface.fillStyle = `rgba(${randomIn(255)}, ${randomIn(255)}, ${randomIn(255)}, 0.5)`
-
-        let x = randomIn(canvas.width)
-        let y = randomIn(canvas.height)
-
-        interface.beginPath()
-        interface.arc(x, y, randomIn(100), 0, Math.PI * 2)
-        interface.fill()
-    }
-*/
