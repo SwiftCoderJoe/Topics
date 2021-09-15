043b1ae15337046092cea280fa7e694261c7ab56
SwiftCoderJoe <coderjoe05@gmail.com>
2021-09-15 17:13:41 -0400

    Add click functionality

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ class Color {
     }
 }
 
+class Coordinate {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
 window.addEventListener("load", (event) => {
 
     // Get a reference to the canvas
@@ -19,6 +26,9 @@ window.addEventListener("load", (event) => {
 
     // Get the canvas interface (Context)
     let interface = canvas.getContext(`2d`)
+
+    // Hold the recent mouse events
+    let mouseEvents = []
 
     // Scale the canvas so that the accessible portion is always 720p
     canvas.width = 1280
@@ -31,63 +41,100 @@ window.addEventListener("load", (event) => {
     // Lines are black
     interface.fillStyle = `rgb(0, 0, 0)`
 
+    // MARK: Draw on the screen
 
-    // MARK: Fill the screen
+    // This block intentionally left blank
 
-    startColor = new Color(255, 255, 0, 1)
-    endColor = new Color(255, 0, 255, 1)
-    drawVShape(0, 1350, 75, 75, 120, 2, 100, 0, startColor, endColor)
+    // MARK: Events
 
-    startColor = new Color(0, 0, 0, 1)
-    endColor = new Color(0, 255, 255, 1)
-    drawVShape(150, 1250, 300, 300, 300, 2, 200, 0, startColor, endColor)
+    canvas.addEventListener(`mouseup`, (ev) => {
+        let rect = canvas.getBoundingClientRect(), // abs. size of element
+            scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+            scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+          
+        let mouseCoords = new Coordinate((ev.clientX - rect.left) * scaleX,
+                                         (ev.clientY - rect.top) * scaleY) 
+        
+        // Add mouse coordinates to the list
+        mouseEvents.unshift(mouseCoords)
 
-    startColor = new Color(0, 0, 0, 0)
-    endColor = new Color(0, 255, 255, 1)
-    drawVShape(150, 1250, 600, 600, 300, 2, 200, 0, startColor, endColor)
+        // Remove last coordinate to limit length to 3
+        if (mouseEvents.length > 3) mouseEvents.pop()        
+    })
 
-    startColor = new Color(0, 255, 255, 1)
-    endColor = new Color(255, 0, 255, 1)
-    drawVShape(0, 1200, 700, 0, 1000, 1, 75, 20, startColor, endColor)
+    window.addEventListener(`keydown`, (ev) => {
+        if (ev.key == ` `) {
+            if (mouseEvents.length == 3) {
+                console.log(`Drawing curve.`)
+                color = new Color(randomIn(255), randomIn(255), randomIn(255), Math.random())
+                drawCurve(mouseEvents[0], mouseEvents[1], mouseEvents[2], randomIn(200), color)
+            } else if (mouseEvents.length == 2) {
+                console.log(`Drawing line.`)
+                startColor = new Color(randomIn(255), randomIn(255), randomIn(255), Math.random())
+                endColor = new Color(randomIn(255), randomIn(255), randomIn(255), Math.random())
+                drawVShape(mouseEvents[0], mouseEvents[1], randomIn(800), 2, randomInRange(10, 30), 0, startColor, endColor)
+            }
+        }
 
+        mouseEvents = []
+    })
 
     // MARK: Functions
 
-    // This function signature is absolutely insane and should use more objects. javascriopt moment!!!
-    function drawVShape(x1, x2, y1, y2, res, width, length, twistRate, startColor, endColor) {
-        let xdiff = x2 - x1
-        let ydiff = y2 - y1
+    function drawCurve(corner1, middle, corner2, res, color) {
 
-        // Get slope and inverse slope to make the V-shapes
-        let slope = -ydiff/xdiff
-        let currentRad = Math.atan(slope)
+        // Account for vertical lines that make the shape seem like it should have 2 more lines
+        res = res + 2
+
+        side1Diff = new Coordinate(corner1.x - middle.x, corner1.y - middle.y)
+        side2Diff = new Coordinate(corner2.x - middle.x, corner2.y - middle.y)
+
+        for (let i = 0; i < res; i++) {
+            let ratio = i/(res - 1)
+
+            lineStartCoordinate = new Coordinate(corner1.x - (ratio * side1Diff.x), corner1.y - (ratio * side1Diff.y))
+            lineEndCoordinate = new Coordinate(middle.x + (ratio * side2Diff.x), middle.y + (ratio * side2Diff.y))
+
+            interface.strokeStyle = color.string
+
+            interface.beginPath()
+            interface.moveTo(lineStartCoordinate.x, lineStartCoordinate.y)
+            interface.lineTo(lineEndCoordinate.x, lineEndCoordinate.y)
+            interface.stroke()
+        }
+    }
+
+    // This function signature is absolutely insane and should use more objects. javascriopt moment!!!
+    function drawVShape(startCoord, endCoord, res, width, length, twistRate, startColor, endColor) {
+
+        let diff = new Coordinate(endCoord.x - startCoord.x, endCoord.y - startCoord.y)
+
+        // Get angle in radians to make the V-shapes
+        let currentRad = Math.atan2(diff.y, diff.x) - (0.5 * Math.PI)
 
         // Color differences
-        let rdiff = endColor.r - startColor.r
-        let gdiff = endColor.g - startColor.g
-        let bdiff = endColor.b - startColor.b
-        let adiff = endColor.a - startColor.a
+        let colorDiff = new Color(endColor.r - startColor.r, endColor.g - startColor.g, endColor.b - startColor.b, endColor.a - startColor.a)
 
         for (let i = 0; i < res; i++) {
             // Linear distribution
             let ratio = i/(res - 1)
     
             // The starting coordinates along the linear distribution
-            x = x1 + ratio * xdiff
-            y = y1 + ratio * ydiff
+            x = startCoord.x + ratio * diff.x
+            y = startCoord.y + ratio * diff.y
 
             // Modulate the angle that the angles come off the line (doesn't actually create realistic twists because of the angle)
-            let rad = 5 * Math.PI/4 + (Math.sin(ratio * twistRate) * Math.PI / 4)
+            let rad = 1 * Math.PI/4 + (Math.sin(ratio * twistRate) * Math.PI / 4)
 
             // First offset point next to the current point along the linear distribution
-            let offsetY1 = y - Math.cos(currentRad - rad) * length
-            let offsetX1 = x - Math.sin(currentRad - rad) * length
+            let offsetY1 = y - Math.sin(currentRad - rad) * length
+            let offsetX1 = x - Math.cos(currentRad - rad) * length
 
             // Second set of coordinates
-            let offsetX2 = x + Math.sin(currentRad + rad) * length
-            let offsetY2 = y + Math.cos(currentRad + rad) * length
-
-            interface.strokeStyle = new Color(startColor.r + ratio * rdiff, startColor.g + ratio * gdiff, startColor.b + ratio * bdiff, startColor.a + ratio * adiff).string
+            let offsetY2 = y + Math.sin(currentRad + rad) * length
+            let offsetX2 = x + Math.cos(currentRad + rad) * length
+            
+            interface.strokeStyle = new Color(startColor.r + ratio * colorDiff.r, startColor.g + ratio * colorDiff.g, startColor.b + ratio * colorDiff.b, startColor.a + ratio * colorDiff.a).string
             interface.lineWidth = width
 
             // Draw the V
@@ -116,5 +163,33 @@ window.addEventListener("load", (event) => {
 })
 
 function randomIn(x) {
-    return Math.floor(Math.random() * (x+ 1))
+    return Math.floor(Math.random() * (x + 1))
 }
+
+function randomInRange(y, x) {
+    return Math.floor(Math.random() * ((x - y) + 1)) + y
+}
+
+
+
+// Mark: Code Graveyard
+
+/*
+
+startColor = new Color(255, 255, 0, 1)
+endColor = new Color(255, 0, 255, 1)
+drawVShape(0, 1350, 75, 75, 120, 2, 100, 0, startColor, endColor)
+
+startColor = new Color(0, 0, 0, 1)
+endColor = new Color(0, 255, 255, 1)
+drawVShape(150, 1250, 300, 300, 300, 2, 200, 0, startColor, endColor)
+
+startColor = new Color(0, 0, 0, 0)
+endColor = new Color(0, 255, 255, 1)
+drawVShape(150, 1250, 600, 600, 300, 2, 200, 0, startColor, endColor)
+
+startColor = new Color(0, 255, 255, 1)
+endColor = new Color(255, 0, 255, 1)
+drawVShape(0, 1200, 700, 0, 1000, 1, 75, 20, startColor, endColor)
+
+*/
